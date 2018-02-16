9d0c1a03a6b6c19e3276b8fe4c4be917d88bffd7
Elizhann <34382987+Elizhann@users.noreply.github.com>
2018-02-16 10:46:01 -0500

    Update pay_calculator.py

--- pay_calculator.py
+++ pay_calculator.py
@@ -1,7 +1,19 @@
-hours = float(input('Enter hours:'))
+#This is a program that calculates base pay
+#It then adds overtime pay for hours worked over 40
 
-rate = float(input('Enter rate:'))
+hours = float(input('Enter hours:')) #Input for hours worked
 
-pay = (rate * 40) + overtime
+rate = float(input('Enter rate:')) #Input for pay rate
 
-print('Pay:$', pay)
+overtime = (rate * 1.5) * (hours - 40) #Calculate overtime as 1.5 * pay for hours worked over 40
+
+#if more than 40 hours were worked, add overtime pay calculation and print both pay and overtime pay
+if hours > 40: 
+    pay = (rate * 40) + overtime
+    print('Pay:$', pay)
+    print('Overtime:$', overtime)
+
+#if 40 or less hours were worked then do simple pay rate calculation and print pay
+else:
+    pay = rate * hours
+    print('Pay:$', pay)
