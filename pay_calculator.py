b25ca64683677dc8bd0e790923b44f0a78dbf5ca
Elizhann <34382987+Elizhann@users.noreply.github.com>
2018-02-16 10:59:16 -0500

    Update pay_calculator.py

--- pay_calculator.py
+++ pay_calculator.py
@@ -1,19 +1,22 @@
-#This is a program that calculates base pay
-#It then adds overtime pay for hours worked over 40
+#This program adds exception handling to to overtime.py program
 
-hours = float(input('Enter hours:')) #Input for hours worked
+try:
+    hours = float(input('Enter hours:')) #Input for hours worked
 
-rate = float(input('Enter rate:')) #Input for pay rate
+    rate = float(input('Enter rate:')) #Input for pay rate
 
-overtime = (rate * 1.5) * (hours - 40) #Calculate overtime as 1.5 * pay for hours worked over 40
+    overtime = (rate * 1.5) * (hours - 40) #Calculate overtime as 1.5 * pay for hours worked over 40
 
-#if more than 40 hours were worked, add overtime pay calculation and print both pay and overtime pay
-if hours > 40: 
-    pay = (rate * 40) + overtime
-    print('Pay:$', pay)
-    print('Overtime:$', overtime)
+    #if more than 40 hours were worked, add overtime pay calculation and print both pay and overtime pay
+    if hours > 40: 
+        pay = (rate * 40) + overtime
+        print('Pay:$', pay)
+        print('Overtime:$', overtime)
 
-#if 40 or less hours were worked then do simple pay rate calculation and print pay
-else:
-    pay = rate * hours
-    print('Pay:$', pay)
+    #if 40 or less hours were worked then do simple pay rate calculation and print pay
+    else:
+        pay = rate * hours
+        print('Pay:$', pay)
+except ValueError:
+    print('Error: Please enter a valid number')
+
