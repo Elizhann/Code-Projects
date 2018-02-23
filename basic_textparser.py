9f4b490be905b6996873f537a93d7b404d67b67b
Elizhann <34382987+Elizhann@users.noreply.github.com>
2018-02-23 16:51:21 -0500

    Update basic_textparser.py

--- basic_textparser.py
+++ basic_textparser.py
@@ -1,18 +1,25 @@
-file_name = input('File Name:')
+def main():
+    file_name = input('File Name:')
 
-open_file = open(file_name, 'r')
+    open_file = open(file_name, 'r')
+    counts = dict()
 
-counts = dict()
+    parse_text(open_file, counts)
 
-for line in open_file:
-    line = line.rstrip()
-    line = line.lower()
-    words = line.split()
-    for word in words:
-        if word not in counts:
-            counts[word] = 1
-        else:
-            counts[word] = counts[word] + 1
+    for key in counts:
+        print(key, counts[key])
 
-for key in counts:
-    print(key, counts[key])
+def parse_text(open_file, counts):
+    for line in open_file:
+        line = line.rstrip()
+        line = line.lower()
+        words = line.split()
+        for word in words:
+            if word not in counts:
+                counts[word] = 1
+            else:
+                counts[word] = counts[word] + 1
+
+    return counts
+
+main()
