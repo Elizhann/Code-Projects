238494fddcad1fa402c63bfea656b8bc5b2f82d3
Elizhann <34382987+Elizhann@users.noreply.github.com>
2018-02-23 16:59:59 -0500

    Add Stopwords
    Added code to ignore stopwords

--- basic_textparser.py
+++ basic_textparser.py
@@ -3,22 +3,26 @@ def main():
 
     open_file = open(file_name, 'r')
     counts = dict()
+    stopwords = ['the','and', 'he','they', 'in', 'as', 'was', 'for', 'by', 'a', 'it', 'at']
 
-    parse_text(open_file, counts)
+    parse_text(open_file, counts,stopwords)
 
     for key in counts:
         print(key, counts[key])
 
-def parse_text(open_file, counts):
+def parse_text(open_file, counts, stopwords):
     for line in open_file:
         line = line.rstrip()
         line = line.lower()
         words = line.split()
         for word in words:
-            if word not in counts:
-                counts[word] = 1
+            if word in stopwords:
+                continue
             else:
-                counts[word] = counts[word] + 1
+                if word not in counts:
+                    counts[word] = 1
+                else:
+                    counts[word] = counts[word] + 1
 
     return counts
 
