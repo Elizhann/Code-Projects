03db9323d456a075c3c0c45db197986aa6c2a37f
Elizhann <34382987+Elizhann@users.noreply.github.com>
2018-02-26 12:30:38 -0500

    Updated code
    Add function for stopwords, stripped punctuation and commented out code

--- basic_textparser.py
+++ basic_textparser.py
@@ -1,29 +1,58 @@
+#   This is a program that opens a text file
+#   Removes common words and counts the number of times unique words appear
+
+import string
+
+#   defines main function
 def main():
-    file_name = input('File Name:')
+    try:
+        file_name = input('File Name:')
 
-    open_file = open(file_name, 'r')
-    counts = dict()
-    stopwords = ['the','and', 'he','they', 'in', 'as', 'was', 'for', 'by', 'a', 'it', 'at']
+        user_file = open(file_name, 'r')
+        counts = dict()
 
-    parse_text(open_file, counts,stopwords)
+        common_words = remove_stopwords()
 
-    for key in counts:
-        print(key, counts[key])
+        words = parse_text(user_file, counts, common_words)
 
-def parse_text(open_file, counts, stopwords):
-    for line in open_file:
+        user_file.close()
+
+        #   print word and number of times appearing
+        for key in counts:
+            print(key, counts[key])
+    except FileNotFoundError:
+        print('Error: File not found')
+
+#   defines function to remove common words ( https://www.ranks.nl/stopwords)
+def remove_stopwords():
+    stopwords_list = open('stopwords.txt', 'r')
+    stopwords = []
+    for line in stopwords_list:
         line = line.rstrip()
-        line = line.lower()
-        words = line.split()
+        stopwords.append(line)
+    return stopwords
+
+#   define function to parse the text file
+def parse_text(user_file, counts, common_words):
+    for line in user_file:
+        line = line.rstrip() #strip newlines
+        line = line.lower() #lowercase
+        
+        #   strip punctuation
+        table = str.maketrans('', '', string.punctuation) 
+        line = line.translate(table)
+
+        words = line.split() #split lines into words
+        
+        #   add unique words to dictionary and count them 
         for word in words:
-            if word in stopwords:
+            if word in common_words:
                 continue
             else:
                 if word not in counts:
                     counts[word] = 1
                 else:
                     counts[word] = counts[word] + 1
-
     return counts
 
 main()
